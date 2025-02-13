81f4d2f60aa549d018a3cfd87847f198a10b3f35
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2025-02-13 16:27:22 +0100

    Update project

--- arduino/esp32/esp32.ino
+++ arduino/esp32/esp32.ino
@@ -1,8 +1,35 @@
+#define solPin A0 // port de connexion du capteur
+#define airPin A3 // port de connexion du capteur
+#define luxPin A6 // port de connexion du capteur
+
+int sol;
+int air;
+int lux;
+
 void setup() {
   Serial.begin(115200);
+
+   pinMode(solPin, INPUT);
+   pinMode(airPin, INPUT);
+   pinMode(luxPin, INPUT);
+}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+   sol = analogRead(solPin);
+   air = analogRead(airPin);
+   lux = analogRead(luxPin);
+   
+   float calc = ((float)sol / 1023) * 100;
+   Serial.print("sol = ");
+   Serial.print(calc); // afficher pourcentage
+   Serial.print("%");
+   Serial.print(" | Val = ");
+   Serial.println(sol); // afficher variable
+   Serial.print("air = ");
+   Serial.println(air); // afficher pourcentage
+   Serial.print("lux = ");
+   Serial.println(lux); // afficher pourcentage
 
+   delay(1000);
 }
--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,33 +1,54 @@
-#define solPin A1 // port de connexion du capteur
-#define airPin A2 // port de connexion du capteur
-#define luxPin A3 // port de connexion du capteur
+#define solPin A1
+#define airPin A2
+#define luxPin A3
+#define RED 3
+#define GREEN 5
+#define BLUE 6
 
 int sol;
 int air;
 int lux;
 
 void setup() {
-   Serial.begin(9600);
-
-   pinMode(solPin, INPUT);
-   pinMode(airPin, INPUT);
-   pinMode(luxPin, INPUT);
+  Serial.begin(9600);
+  
+  pinMode(solPin, INPUT);
+  pinMode(airPin, INPUT);
+  pinMode(luxPin, INPUT);
+  
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, HIGH);
 }
 
 void loop() {
+   
    sol = analogRead(solPin);
    air = analogRead(airPin);
    lux = analogRead(luxPin);
+  
+   analogWrite(RED, 255);
+   analogWrite(BLUE, 255);
+   analogWrite(GREEN, 255);
    
    float calc = ((float)sol / 1023) * 100;
    Serial.print("sol = ");
    Serial.print(calc); // afficher pourcentage
-   Serial.print("%");
-   Serial.print(" | Val = ");
+   Serial.print("% | Val = ");
    Serial.println(sol); // afficher variable
+   calc = ((float)air / 1023) * 100;
    Serial.print("air = ");
+   Serial.print(calc); // afficher pourcentage
+   Serial.print("% | Val = ");
    Serial.println(air); // afficher pourcentage
+   calc = ((float)lux / 1023) * 100;
    Serial.print("lux = ");
+   Serial.print(calc); // afficher pourcentage
+   Serial.print("% | Val = ");
    Serial.println(lux); // afficher pourcentage
 
    delay(1000);
