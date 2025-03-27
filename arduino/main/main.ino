f6513dad41a4ffbec382b27585fcf09d7a825b0a
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2025-03-27 13:51:58 +0100

    Better code + debug baud

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -13,8 +13,8 @@ int air;
 int lux;
 
 void setup() {
-  Serial.begin(115200);
-  arduinoSerial.begin(9600);   // Communication avec l'ESP8266
+  Serial.begin(115200);  // Debug sur le moniteur série
+  arduinoSerial.begin(4800);   // Communication avec l'ESP8266
   
   pinMode(solPin, INPUT);
   pinMode(airPin, INPUT);
@@ -40,22 +40,39 @@ void loop() {
    analogWrite(GREEN, 255);
    
    float calc_sol = ((float)sol / 1023) * 100;
+   float calc_air = ((float)air / 1023) * 100;
+   float calc_lux = ((float)lux / 1023) * 100;
+
+   //Information capteur d'humidité des sols
    Serial.print("sol = ");
    Serial.print(calc_sol); // afficher pourcentage
    Serial.print("% | Val = ");
    Serial.println(sol); // afficher variable
-   float calc_air = ((float)air / 1023) * 100;
+
+   //Informations capteur d'humidité de l'air
    Serial.print("air = ");
    Serial.print(calc_air); // afficher pourcentage
    Serial.print("% | Val = ");
    Serial.println(air); // afficher pourcentage
-   float calc_lux = ((float)lux / 1023) * 100;
+
+   //Informations capteur de lux
    Serial.print("lux = ");
    Serial.print(calc_lux); // afficher pourcentage
    Serial.print("% | Val = ");
    Serial.println(lux); // afficher pourcentage
+   
+   // Construction propre du JSON
+   String jsonData = "{\"device\": \"raspberry_device\", \"data\": {";
+   jsonData += "\"lux\": " + String(calc_lux, 2) + ",";
+   jsonData += "\"humidity_air\": " + String(calc_air, 2) + ",";
+   jsonData += "\"humidity_sol\": " + String(calc_sol, 2);
+   jsonData += "}}";
 
-   arduinoSerial.println("[{\"device\": \"raspberry_device\",\"data\": {\"lux\": "+ String(calc_lux) +",\"humidity_air\": "+ String(calc_air) +",\"humidity_sol\": "+ String(calc_sol) +"}}]");
+   // Affichage pour le debug
+   Serial.println("Envoi JSON: " + jsonData);
 
+   // Envoi des données à l'ESP8266
+   arduinoSerial.println(jsonData);
+   
    delay(1000);
 }
