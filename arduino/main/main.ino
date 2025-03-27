d3e1d5624bf5b0ef59d7e3cef46c02970d86748f
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2025-03-27 16:25:03 +0100

    Add Thermoresistor

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,9 +1,19 @@
 #define solPin A1
 #define airPin A2
 #define luxPin A3
+#define gazPin A4
+#define thermaPin A5
+
 #define RED 5
 #define GREEN 6
 #define BLUE 9
+
+#define SERIES_RESISTOR 100000.0  // Résistance fixe de 100 kΩ
+#define VCC 5.0  // Tension d'alimentation Arduino
+
+#define A 0.0009032679
+#define B 0.000248772
+#define C 0.0000002041094
 
 #include <SoftwareSerial.h>
 
@@ -11,6 +21,8 @@ SoftwareSerial arduinoSerial(2, 3); // RX sur D2, TX sur D3
 int sol;
 int air;
 int lux;
+int gaz;
+int rawValue;
 
 void setup() {
   Serial.begin(115200);  // Debug sur le moniteur série
@@ -19,6 +31,8 @@ void setup() {
   pinMode(solPin, INPUT);
   pinMode(airPin, INPUT);
   pinMode(luxPin, INPUT);
+  pinMode(gazPin, INPUT);
+  pinMode(thermaPin, INPUT);
   
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
@@ -27,6 +41,10 @@ void setup() {
   digitalWrite(RED, HIGH);
   digitalWrite(GREEN, HIGH);
   digitalWrite(BLUE, HIGH);
+  
+  analogWrite(RED, 0);
+  analogWrite(BLUE, 0);
+  analogWrite(GREEN, 0);
 }
 
 void loop() {
@@ -34,14 +52,29 @@ void loop() {
    sol = analogRead(solPin);
    air = analogRead(airPin);
    lux = analogRead(luxPin);
-  
-   analogWrite(RED, 255);
-   analogWrite(BLUE, 255);
-   analogWrite(GREEN, 255);
+   gaz = analogRead(gazPin);
+   rawValue = analogRead(thermaPin);
+   
+   float voltage = (rawValue / 1023.0) * VCC;
+   float resistance = SERIES_RESISTOR * (voltage / (VCC - voltage));
+   float lnR = log(resistance);
+   float temperatureKelvin = 1.0 / (A + (B * lnR) + (C * lnR * lnR * lnR));
    
    float calc_sol = ((float)sol / 1023) * 100;
    float calc_air = ((float)air / 1023) * 100;
    float calc_lux = ((float)lux / 1023) * 100;
+   float calc_gaz = ((float)gaz / 1023) * 100;
+   float calc_temp = ((3950.0 / (log(resistance / 100000.0) + (3950.0 / 298.15))) - 273.15) * -1;
+
+   if(calc_gaz < 25) {
+     analogWrite(RED, 255);
+     analogWrite(BLUE, 255);
+     analogWrite(GREEN, 255);
+   } else {
+     analogWrite(RED, 0);
+     analogWrite(BLUE, 0);
+     analogWrite(GREEN, 0);
+   }
 
    //Information capteur d'humidité des sols
    Serial.print("sol = ");
@@ -60,12 +93,24 @@ void loop() {
    Serial.print(calc_lux); // afficher pourcentage
    Serial.print("% | Val = ");
    Serial.println(lux); // afficher pourcentage
+
+   //Informations capteur de lux
+   Serial.print("gaz = ");
+   Serial.print(calc_gaz); // afficher pourcentage
+   Serial.print("% | Val = ");
+   Serial.println(gaz); // afficher pourcentage
+   
+   //Informations capteur de température
+   Serial.print("temp = ");
+   Serial.print(calc_temp);
+   Serial.println("°C");
    
    // Construction propre du JSON
    String jsonData = "{\"device\": \"raspberry_device\", \"data\": {";
    jsonData += "\"lux\": " + String(calc_lux, 2) + ",";
    jsonData += "\"humidity_air\": " + String(calc_air, 2) + ",";
    jsonData += "\"humidity_sol\": " + String(calc_sol, 2);
+   jsonData += "\"gaz\": " + String(calc_gaz, 2);
    jsonData += "}}";
 
    // Affichage pour le debug
